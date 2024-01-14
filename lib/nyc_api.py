578bff07ba558bc6ab44605e678956b0b8c08456
bill <bill.arnold@student.moringaschool.com>
2024-01-14 18:34:26 +0300

    done

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,22 @@
-
 import requests
 import json
 
 class GetPrograms:
+    def __init__(self, api_url):
+        self.api_url = api_url
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_response_body(self):
+        response = requests.get(self.api_url)
+        return response.content
 
-    response = requests.get(URL)
-    return response.content
+    def load_data(self):
+        return self.get_response_body()
 
-programs = GetPrograms().get_programs()
-print(programs)
+if __name__ == "__main__":
+    # Replace the API endpoint with the actual URL from the API documentation
+    api_url = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    
+    programs = GetPrograms(api_url)
+    response_body = programs.load_data()
+
+    print(response_body.decode('utf-8'))
